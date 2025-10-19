9240c4ae704cedba2052c8e99301b5dba9ffafd6
resilva93 <repesi93@yahoo.com.br>
2025-10-19 11:15:46 -0300

    Subindo a primeira versao do projeto - 20251019_v1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("O resultado da soma é:");
+
+        }
+    }
+}
