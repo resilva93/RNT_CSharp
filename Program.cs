57c27171614d958744967a4b52584686207e3230
resilva93 <repesi93@yahoo.com.br>
2025-10-19 17:02:12 -0300

    Calculadora.csproj Calculadora.sln Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Intrinsics.X86;
 
 namespace Calculadora
 {
@@ -13,8 +14,34 @@ namespace Calculadora
             Console.WriteLine("Digite o segundo valor: ");
             float v2 = float.Parse(Console.ReadLine());
 
+            Console.WriteLine("Escolha a operação desejada:");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+
+            int operacao = int.Parse(Console.ReadLine());
+            float resultado = 0;
+            switch (operacao)
+            {
+                case 1:
+                    resultado = v1 + v2;
+                    break;
+                case 2:
+                    resultado = v1 - v2;
+                    break;
+                case 3:
+                    resultado = v1 * v2;
+                    break;
+                case 4:
+                    resultado = v1 / v2;
+                    break;
+                default:
+                    Console.WriteLine("Operação inválida!");
+                    break;
+            }
             Console.WriteLine("");
-            Console.WriteLine("O resultado da soma é:");
+            Console.WriteLine("O resultado é: "+ resultado);
 
         }
     }
